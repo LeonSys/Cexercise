4ae61fbb511d623347d3f6c70455d0f97ce0b187
LeonSys <32563674+LeonSys@users.noreply.github.com>
2017-11-21 15:25:35 +0100

    Add files via upload

---
+++ Vectorclass.cpp
@@ -0,0 +1,43 @@
+//
+//  Vectorclass.cpp
+//  ObjectC++
+//
+//  Created by Horvath, Leon on 2017. 11. 21..
+//  Copyright © 2017. Horvath, Leon. All rights reserved.
+//
+#include <iostream>
+#include <stdlib.h>
+#include <math.h>
+#include <stdio.h>
+class Vector {
+    
+private:     //Points of the Class
+    float x;
+    float y;
+    float z;
+    
+public: Vector(){ //Giving value to the points
+    
+    x=rand()%100+1;
+    y=rand()%100+1;
+    z=rand()%100+1;
+    
+}
+    
+    void kiad(){  //The printf function
+        printf("A pont koordinátái: x=%2f y=%2f z=%2f \n", x, y ,z);
+    };
+    
+    
+    
+    double tav(Vector v){ //Calculate distance function
+        
+        
+        double d = sqrt(pow(x-v.x,2) + pow(y - v.y,2) + pow(z-v.z,2));
+        return d;
+        
+    };
+    
+    
+    
+};
